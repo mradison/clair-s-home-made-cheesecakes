bfefdd36240f136122f23ae6891b3763da6a80df
Madison Reynolds <madison.reynolds@phoenix.edu>
2024-04-03 22:33:28 -0700

    created starter website for cheesecake business

---
+++ client/src/pages/homepage.jsx
@@ -0,0 +1,57 @@
+import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+import OldroomJPG from "../assets/oldroom.jpg";
+import HalfwayroomJPG from "../assets/halfway.jpg";
+import NewroomJPG from "../assets/newroom.jpg";
+
+function Homepage({ TasksValue }) {
+  return (
+    <main className="main">
+      <div className="welcome">
+      <h1 style={{ textDecoration: "underline" }}>Welcome to Clair's Home Made Cheesecakes</h1>
+      </div>
+
+      <div className="friend">
+        <h3>Your Friend in the Cheesecake Business</h3>
+      </div>
+
+      <section className="ourServices">
+        <h2 style={{ textDecoration: "underline" }}>Our Cheesecake Categories</h2>
+        <ul>
+          <p>Single layered - 8 inch $20, or 9-10 inch $30: Each cheesecake comes with its standard crust. You can choose your own choice of crust from original graham cracker, chocolate graham cracker, cinnamon graham cracker, vanilla wafer, shortbread, gingerbread, crumble crust (like you would find on a pie or cobbler), brownie, Oreo or chocolate chip cookie crust for an additional $5.00. </p>
+          <p>Multi-Layered - 8 inch $30 and 9-10 $40</p>
+          <p>Cheesecake Cupcakes – 12 cupcakes for $25 or 24 cupcakes for $35</p>
+          <p>Jumbo Cheesecake Cupcakes – 6 cupcakes for $20 or 12 cupcakes for $30</p>
+          <p>Mini Cheesecake Cupcakes – 24 cupcakes for $15 or 48 cupcakes for $25</p>
+          <p>Cheesecake Bars 2x2 – 16 for $20 or 24 bars for $30 or 48 bars for $40</p>
+          <p>Cheesecake Bars 1x1 – 64 for $20 or 96 bars for $30</p>
+          <p>“All about the Crust” – This is cheesecake encased in graham cracker crust! - 8 inch $20 or 9-10 $30 add $5 dollars and I will add in the flavor or topping of your choice</p>
+          <p>Seasonal Favorites or anytime you like - 8 inch $20 or 9-10 inch $30</p>
+        </ul>
+      </section>
+
+      {/* <section className="portfolio">
+        <h2 style={{ textDecoration: "underline" }}>C</h2>
+        <div className="imageContainer">
+        <p className="LCText">Before</p>
+          <div className="imageItem">
+            <img className="frontPics" src={OldroomJPG} alt="Before Picture" />
+          </div>
+          <p className="LCText">During</p>
+          <div className="imageItem">
+            <img
+              className="frontPics"
+              src={HalfwayroomJPG}
+              alt="Halfway Picture"
+            />
+          </div>
+          <p className="LCText">After</p>
+          <div className="imageItem">
+            <img className="frontPics" src={NewroomJPG} alt="After Picture" />
+          </div>
+        </div>
+      </section> */}
+    </main>
+  );
+}
+export default Homepage;
---
+++ client/src/pages/servicespage.jsx
@@ -0,0 +1,93 @@
+function Servicespage({ TasksValue }) {
+    return (
+      <main className="main">
+
+<section id="services">
+<h1 style={{ textDecoration: "underline" }}>Our Flavors</h1>
+<div className="residential">
+    <h3>Single Layered</h3>
+      <ul>
+        <li>Caramel Turtle – Chocolate cheesecake w/chocolate, caramel & pecans! </li>
+        <li>NY Style – It’s a classic! </li>
+        <li>Banana Cream – Banana cream cheesecake w/fresh whipped cream </li>
+        <li>Banana Cream #2 – Banana cream cheesecake w/ a sweet sour cream topping baked right on! </li>
+        <li>Bananas Fosters – Banana cheesecake top with banana fosters! </li>
+        <li>Banoffee - Banana cheesecake with homemade toffee sauce & pecans! </li>
+        <li>Caramel Covered Banana cream - </li>
+        <li>Chocolate covered Banana cream - </li>
+        <li>Chocolate Truffle – Chocolate cheesecake encased in chocolat</li>
+        <li>Chocolate Caramel Truffle - </li>
+        <li>Peanut Butter – It’s loaded with peanut butter goodness! </li>
+        <li>Chocolate Peanut Butter Cup – chocolate cheesecake filled and topped w/peanut butter cups chocolate ganache! </li>
+        <li>Double Peanut Butter/Peanut Butter Cup – Peanut butter cheesecake filled and topped w/peanut butter cups and chocolate ganache! </li>
+        <li>Peanut Butter Cup/Butterfinger Caramel Fudge – New style cheesecake layered and topped with pieces of peanut butter cups, butterfingers and caramel then it is topped with fudge and peanut butter/buttercream! </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>Multi-Layered</h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>Cheesecake Cupcakes</h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>Jumbo Cheesecake Cupcakes</h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>Mini Cheesecake Cupcakes</h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>Cheesecake Bars 2x2</h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>Cheesecake Bars 1x1 </h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>“All about the Crust” – This is cheesecake encased in graham cracker crust!</h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+
+<div className="renovation">
+    <h3>Seasonal Favorites or anytime you like</h3>
+      <ul>
+        <li></li>
+      </ul>
+</div>
+</section>
+
+</main>
+  );
+}
+export default Servicespage;
