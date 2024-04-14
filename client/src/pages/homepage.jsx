956eaae7f9a8b80d239be407054907e93eb0c6d2
Madison Reynolds <madison.reynolds@phoenix.edu>
2024-04-13 23:22:20 -0700

    changed path to try to fix error

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,5 +1,5 @@
-// import { useQuery } from "@apollo/client";
-// import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 import Strawberryswirl from "../assets/strawberryswirl.jpg";
 import Carrotcake from "../assets/carrotcake.jpg";
 import Caramelturtlecupcakes from "../assets/Caramelturtlecupcakes.jpg";
